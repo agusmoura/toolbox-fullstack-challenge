44673a2616fa89458f0725fb142ed22c53bb2522
Agustin Mouriño <agus442m@gmail.com>
2025-10-18 01:30:11 +0000

    refactor: standardize quotes and improve webpack configuration formatting

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,48 +1,54 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js',
-    clean: true
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.[contenthash].js",
+    clean: true,
   },
-  mode: process.env.NODE_ENV || 'development',
-  devtool: 'source-map',
+  mode: process.env.NODE_ENV || "development",
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html'
+      template: "./public/index.html",
+      filename: "index.html",
     }),
     new webpack.DefinePlugin({
-      'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL),
-      'process.env.REACT_APP_API_TIMEOUT': JSON.stringify(process.env.REACT_APP_API_TIMEOUT || '10000'),
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-    })
+      "process.env.REACT_APP_API_URL": JSON.stringify(
+        process.env.REACT_APP_API_URL
+      ),
+      "process.env.REACT_APP_API_TIMEOUT": JSON.stringify(
+        process.env.REACT_APP_API_TIMEOUT || "10000"
+      ),
+      "process.env.NODE_ENV": JSON.stringify(
+        process.env.NODE_ENV || "development"
+      ),
+    }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public')
+      directory: path.join(__dirname, "public"),
     },
     port: 3001,
     hot: true,
@@ -50,10 +56,10 @@ module.exports = {
     historyApiFallback: true,
     proxy: [
       {
-        context: ['/files'],
-        target: 'http://localhost:3000',
-        changeOrigin: true
-      }
-    ]
-  }
+        context: ["/files"],
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    ],
+  },
 };
