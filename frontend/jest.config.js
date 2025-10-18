dad0c1bc711694f8dc64f36565676c0ddb67667c
Agustin Mouriño <agus442m@gmail.com>
2025-10-18 00:47:03 +0000

    Add frontend testing and linting configuration
    Add ESLint configuration with React best practices.
    Add Jest configuration with coverage thresholds.
    Add test setup file for React Testing Library.
    Add .env.example for frontend environment variables.

---
+++ frontend/jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/src/__mocks__/fileMock.js'
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/index.js',
+    '!src/store/index.js',
+    '!src/**/*.test.js',
+    '!src/setupTests.js'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  },
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)'
+  ]
+};
